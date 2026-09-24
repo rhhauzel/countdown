29e0b69ffffed02fe78039e6fc589a756c962e96
Raphael Hauzel <rhhauzel@gmail.com>
2026-09-24 14:56:01 +0530

    tablet making

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import CountdownTimer from "@/components/CountdownTimer";
 import { format } from "date-fns";
+import { connection } from "next/server";
 import Image from "next/image";
 
-export default function Page() {
+export default async function Page() {
  const Keimah = "2026-12-21T08:55:00";
   const Tei = "2026-12-23T17:00:00";
   const Basil = "2026-12-19T16:00:00";
   const Eli = "2026-12-15T08:00:00";
+
+  await connection();
  
    return (
      <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-white dark:bg-gray-900 transition-colors">
@@ -14,9 +17,11 @@ export default function Page() {
          <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl text-gray-900 dark:text-white">
            We are We are going home soon soon
          </h1>
-         <p className="text-lg text-gray-600 dark:text-gray-300">
-           {format(Date.now(),"dd/MM/yyyy")}
-         </p>
+         <div className="flex justify-between">
+          <div>{format(new Date(2026, 8, 19),"dd/MM/yyyy")}</div>
+          <div></div>
+          <div>{format(Date.now(), "dd/MM/yyyy")}</div>
+         </div>
          
          <div className="mt-8">
            Keimah - <CountdownTimer targetDate={Keimah} />
